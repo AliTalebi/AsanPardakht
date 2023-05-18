a12578fbc59e3f52e77ff31ff62b024a71bbffc3
ali talebi <AliTalebiKondori@Gmail.com>
2023-05-18 21:05:41 +0330

    handle ExecuteAsync exceptions

--- AsanPardakht.WorkerService.MessageProcessorConsoleApp/SyncDataWorkerService.cs
+++ AsanPardakht.WorkerService.MessageProcessorConsoleApp/SyncDataWorkerService.cs
@@ -1,19 +1,29 @@
 ﻿using Microsoft.Extensions.Hosting;
 using AsanPardakht.WorkerService.MessageProcessorConsoleApp.Outbox;
+using Microsoft.Extensions.Logging;
 
 namespace AsanPardakht.WorkerService.MessageProcessorConsoleApp
 {
     public class SyncDataWorkerService : BackgroundService
     {
+        private readonly ILogger _logger;
         private readonly IReadOutboxEventTask _outboxEventHandler;
-        public SyncDataWorkerService(IReadOutboxEventTask outboxEventHandler)
+        public SyncDataWorkerService(IReadOutboxEventTask outboxEventHandler,ILogger<SyncDataWorkerService> logger)
         {
+            _logger = logger;
             _outboxEventHandler = outboxEventHandler;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            await _outboxEventHandler.StartAsync();
+            try
+            {
+				await _outboxEventHandler.StartAsync();
+			}
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "exception occurred when calling Startasync method ");
+            }
         }
     }
 }
